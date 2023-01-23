59ef0c9a5e42a8a424a450f619663b8e806ad310
meshkatul <jannat.m.bd@gmail.com>
2023-01-23 13:18:02 -0600

    backend deployment assessment module 40.7

---
+++ src/categories/categories.controller.js
@@ -0,0 +1,16 @@
+const categoriesService = require("./categories.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function list(req, res, next) {
+  try {
+    const data = await categoriesService.list();
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+};
+
